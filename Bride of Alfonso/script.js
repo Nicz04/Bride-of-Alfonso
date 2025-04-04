776a059f5c9647440859f5e98a6c96bd6d372686
Nicz04 <bolfangon@gmail.com>
2025-04-04 20:28:33 +0800

    Add files via upload

---
+++ Bride of Alfonso/script.js
@@ -0,0 +1,14 @@
+alert("Welcome to my website!")
+prompt("Do you like JavaScript?")
+prompt("What is your name?")
+
+if (prompt === "yes") {
+    alert("Hello, I hope you will like it soon!")
+}
+
+else {
+    alert("Hello, Glad you like JavaScript!")
+}
+
+document.getElementById("output").textContent = message
+
